d5e3c412d02ee4c5678af29bd722c012055e7ad1
Sheriff Amzat <kunmiamzat1@gmail.com>
2021-11-03 14:42:50 +0100

    Finished projects

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('email');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', text => {
+    text.preventDefault();
+
+    let inputValue = input.value;
+    if(!validateEmail(inputValue)) {
+        return form.classList.add('error')
+
+    } else {
+        return form.classList.remove('error')
+    }
+});
+
+
+
+function validateEmail(email) {
+    let correctEmail = /\w+@[a-z].[a-z]/;
+    return correctEmail.test(email);
+}
